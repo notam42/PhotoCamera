3911c24dff4d6dbcce20047881ab2e2a5f5ba601
Hovik Melikyan <hovik.melikyan@gmail.com>
2025-01-11 12:41:08 +0100

    Reorganized source tree

--- AVCam/UIComponents/ViewExtensions.swift
+++ AVCam/UIComponents/ViewExtensions.swift
@@ -24,32 +24,6 @@ extension PlatformView {
     var isCompactSize: Bool { horizontalSizeClass == .compact || verticalSizeClass == .compact }
 }
 
-/// A container view for the app's toolbars that lays the items out horizontally
-/// on iPhone and vertically on iPad and Mac Catalyst.
-struct AdaptiveToolbar<Content: View>: PlatformView {
-    
-    @Environment(\.verticalSizeClass) var verticalSizeClass
-    @Environment(\.horizontalSizeClass) var horizontalSizeClass
-    
-    private let horizontalSpacing: CGFloat
-    private let verticalSpacing: CGFloat
-    private let content: Content
-    
-    init(horizontalSpacing: CGFloat = 0.0, verticalSpacing: CGFloat = 0.0, @ViewBuilder content: () -> Content) {
-        self.content = content()
-        self.horizontalSpacing = horizontalSpacing
-        self.verticalSpacing = verticalSpacing
-    }
-    
-    var body: some View {
-        if isRegularSize {
-            VStack(spacing: verticalSpacing) { content }
-        } else {
-            HStack(spacing: horizontalSpacing) { content }
-        }
-    }
-}
-
 struct DefaultButtonStyle: ButtonStyle {
     
     @Environment(\.isEnabled) private var isEnabled: Bool
@@ -80,13 +54,6 @@ struct DefaultButtonStyle: ButtonStyle {
     
     var isRegularSize: Bool {
         horizontalSizeClass == .regular && verticalSizeClass == .regular
-    }
-}
-
-extension View {
-    func debugBorder(color: Color = .red) -> some View {
-        self
-            .border(color)
     }
 }
 
